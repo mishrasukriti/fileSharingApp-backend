f0d0cea833c6099971212005a3a84b6f0172f2a3
Sukriti Mishra <sukritippl@gmail.com>
2021-01-31 21:44:02 +0530

    Performing Some changes

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
-// const corsOptions = {
-//   origin: process.env.ALLOWED_CLIENTS.split(',')
-//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-// }
 
-// Default configuration looks like
+// Default configuration 
 const corsOptions ={
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
